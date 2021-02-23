65dd55d177ac32133aac3394f21704040f4a578a
saska <saska@oeoe.fi>
2021-02-23 21:03:06 +0200

    style most things

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,57 @@
   <div class="app">
     <div class="container">
       <div class="app__title">
-        Qtion<br>
+        Qtion
+        <br>
         kalabloki
       </div>
 
-      <div
+      <Message
         v-for="message in messages"
         :key="message.id"
-      >
-        {{ message.id }}
-        {{ message.author }}
-        {{ message.name }}
-        {{ message.message }}
-      </div>
+        :message="message.message"
+        :author="message.author"
+        :time="message.time"
+        class="app__message"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import messages from './qtion-kalabloki.json';
+import messages from '@/qtion-kalabloki.json';
+import Message from '@/components/Message';
+
+const tags = {
+  img: (size, url) => size
+    ? `<img src="${url}">`
+    : `<img src="${url}" width="${size}">`,
+  quote: (sourceId, body) => {
+    const sourceMessage = messages.find((message) => message.id === parseInt(sourceId));
+    console.log(sourceId)
+    console.log(messages);
+    return `<figure><figcaption>${sourceMessage.author}</figcaption><blockquote>${body}</blockquote></figure>`;
+  },
+  url: (url, body) => `<a href="${url}">${body}</a>`,
+}
+
+const messagesParsed = messages.map((objMessage) => {
+  const tagsOr = Object.keys(tags).join('|');
+  const regex = new RegExp(`\\[(${tagsOr})=?(.*?)](.*?)\\[\\/\\1]`, 'gs');
+  return {
+    ...objMessage,
+    message: objMessage.message.replaceAll(regex, (match, tag, ...args) => tags[tag](...args)),
+  }
+})
 
 export default {
   name: 'App',
+  components: {
+    Message,
+  },
   data() {
     return {
-      messages,
+      messages: messagesParsed,
     }
   },
 }
@@ -44,12 +70,23 @@ export default {
 }
 
 .app__title {
-  font-size: r(64);
-  font-weight: 100;
   margin-bottom: r(32);
+  font-size: r(64);
+  font-weight: 700;
 
   @include breakpoint(S) {
+    margin-bottom: r(64);
     font-size: r(80);
+  }
+}
+
+.app__message {
+  & + & {
+    margin-top: r(24);
+
+    @include breakpoint(S) {
+      margin-top: r(40);
+    }
   }
 }
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="message">
+    <div class="message__meta">
+      <div class="message__author">{{ author }}</div>
+      <div class="message__time">{{ time }}</div>
+    </div>
+    <div
+      class="message__message"
+      v-html="message"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  props: {
+    author: String,
+    message: String,
+    time: String,
+  },
+}
+</script>
+
+<style lang="scss">
+@import "~@/styles/definitions";
+
+.message {
+  padding: r(24);
+  background-color: #FAFAFA;
+
+  @include breakpoint(SM) {
+    padding: r(48);
+    font-size: r(18);
+  }
+}
+
+.message__meta {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: r(16);
+}
+
+.message__author {
+  font-weight: 700;
+}
+
+.message__time {
+  font-size: r(12);
+  color: #AAAAAA;
+}
+
+.message__message {
+  white-space: pre-line;
+  line-height: 1.65;
+
+  figure {
+    font-size: r(12);
+    padding-left: r(8);
+    border-left: r(1) solid $color-black;
+  }
+
+  figcaption {
+    font-weight: 700;
+  }
+
+  img {
+    max-width: 100%;
+  }
+}
+</style>
